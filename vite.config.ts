16165d9f6c541ba3a1e244d6cfcbc9806cee72eb
Olivia <109307370+nshebang@users.noreply.github.com>
2025-07-28 20:40:01 -0600

    bypass unused value checks

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  esbuild: {
+    // this disables syntax-level unused warning from esbuild
+    logOverride: { 'this-is-undefined-in-esm': 'silent' },
+    tsconfigRaw: {
+      compilerOptions: {
+        noUnusedLocals: false,
+        noUnusedParameters: false
+      }
+    }
+  }
 })
